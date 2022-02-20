be6bb1582131bcb058280b3424c7d15fb9f7cfa6
Michal78900 <michal.dlugopis@gmail.com>
2022-02-20 01:50:30 +0100

    Doc

--- MapEditorReborn/API/Extensions/ToolsExtensions.cs
+++ MapEditorReborn/API/Extensions/ToolsExtensions.cs
@@ -10,9 +10,14 @@
         /// Gets a value indicating whether the specified <see cref="Item"/> is a ToolGun.
         /// </summary>
         /// <param name="item">The <see cref="Item"/> to check.</param>
-        /// <returns><see langword="true"/> if the <paramref name="item"/> is a Tool Gun; otherwise, <see langword="false"/>.</returns>
+        /// <returns><see langword="true"/> if the <paramref name="item"/> is a ToolGun; otherwise, <see langword="false"/>.</returns>
         public static bool IsToolGun(this Item item) => item != null && ToolGuns.ContainsKey(item.Serial);
 
+        /// <summary>
+        /// Gets a value indicating whether the specified <see cref="Item"/> is a GravityGun.
+        /// </summary>
+        /// <param name="item">The <see cref="Item"/> to check.</param>
+        /// <returns><see langword="true"/> if the <paramref name="item"/> is a GravityGun; otherwise, <see langword="false"/>.</returns>
         public static bool IsGravityGun(this Item item) => item != null && GravityGuns.Contains(item.Serial);
     }
 }
