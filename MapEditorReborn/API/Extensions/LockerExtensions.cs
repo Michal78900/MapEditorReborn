0fc7ed227d49d00261b50ffc2af6de72a8b24fbe
Nao <60253860+NaoUnderscore@users.noreply.github.com>
2022-03-30 15:48:18 +0200

    LockerExtensions docs +overload

--- MapEditorReborn/API/Extensions/LockerExtensions.cs
+++ MapEditorReborn/API/Extensions/LockerExtensions.cs
@@ -9,8 +9,17 @@
     using Mirror;
     using UnityEngine;
 
+    /// <summary>
+    /// A set of useful extensions to easily interact with in-game lockers.
+    /// </summary>
     public static class LockerExtensions
     {
+        /// <summary>
+        /// Spawns an item inside the locker.
+        /// </summary>
+        /// <param name="lockerChamber">The <see cref="LockerChamber"/> into which spawn the item.</param>
+        /// <param name="item">The name of the item.</param>
+        /// <param name="amount">The amount of items to spawn.</param>
         public static void SpawnItem(this LockerChamber lockerChamber, string item, uint amount)
         {
             if (Enum.TryParse(item, true, out ItemType parsedItem))
@@ -30,13 +39,9 @@
                     (itemPickupBase as IPickupDistributorTrigger)?.OnDistributed();
 
                     if (lockerChamber._spawnOnFirstChamberOpening)
-                    {
                         lockerChamber._toBeSpawned.Add(itemPickupBase);
-                    }
                     else
-                    {
                         ItemDistributor.SpawnPickup(itemPickupBase);
-                    }
                 }
 
                 return;
@@ -57,42 +62,44 @@
                     (itemPickupBase as IPickupDistributorTrigger)?.OnDistributed();
 
                     if (lockerChamber._spawnOnFirstChamberOpening)
-                    {
                         lockerChamber._toBeSpawned.Add(itemPickupBase);
-                    }
                     else
-                    {
                         ItemDistributor.SpawnPickup(itemPickupBase);
-                    }
                 }
             }
         }
 
-        public static LockerType GetLockerTypeByName(this Locker locker)
-        {
-            return locker.name switch
-            {
-                "Scp268PedestalStructure Variant(Clone)" => LockerType.Pedestal,
-                "LargeGunLockerStructure(Clone)" => LockerType.LargeGun,
-                "RifleRackStructure(Clone)" => LockerType.RifleRack,
-                "MiscLocker(Clone)" => LockerType.Misc,
-                "RegularMedkitStructure(Clone)" => LockerType.Medkit,
-                _ => LockerType.Adrenaline,
-            };
-        }
+        /// <summary>
+        /// Gets the <see cref="LockerType"/> from the given <see cref="Locker"/> object.
+        /// </summary>
+        /// <param name="locker">The <see cref="Locker"/> to check.</param>
+        /// <returns>The corresponding <see cref="LockerType"/>.</returns>
+        public static LockerType GetLockerType(this Locker locker) => locker.name.GetLockerTypeByName();
 
-        public static GameObject GetLockerObjectByType(this LockerType doorType)
+        /// <summary>
+        /// Gets the <see cref="LockerType"/> by name.
+        /// </summary>
+        /// <param name="locker">The name to check.</param>
+        /// <returns>The corresponding <see cref="LockerType"/>.</returns>
+        public static LockerType GetLockerTypeByName(this string name) => name switch
         {
-            return doorType switch
-            {
-                LockerType.Pedestal => ObjectType.PedestalLocker.GetObjectByMode(),
-                LockerType.LargeGun => ObjectType.LargeGunLocker.GetObjectByMode(),
-                LockerType.RifleRack => ObjectType.RifleRackLocker.GetObjectByMode(),
-                LockerType.Misc => ObjectType.MiscLocker.GetObjectByMode(),
-                LockerType.Medkit => ObjectType.MedkitLocker.GetObjectByMode(),
-                LockerType.Adrenaline => ObjectType.AdrenalineLocker.GetObjectByMode(),
-                _ => null,
-            };
-        }
+            "Scp268PedestalStructure Variant(Clone)" => LockerType.Pedestal,
+            "LargeGunLockerStructure(Clone)" => LockerType.LargeGun,
+            "RifleRackStructure(Clone)" => LockerType.RifleRack,
+            "MiscLocker(Clone)" => LockerType.Misc,
+            "RegularMedkitStructure(Clone)" => LockerType.Medkit,
+            _ => LockerType.Adrenaline,
+        };
+
+        public static GameObject GetLockerObjectByType(this LockerType doorType) => doorType switch
+        {
+            LockerType.Pedestal => ObjectType.PedestalLocker.GetObjectByMode(),
+            LockerType.LargeGun => ObjectType.LargeGunLocker.GetObjectByMode(),
+            LockerType.RifleRack => ObjectType.RifleRackLocker.GetObjectByMode(),
+            LockerType.Misc => ObjectType.MiscLocker.GetObjectByMode(),
+            LockerType.Medkit => ObjectType.MedkitLocker.GetObjectByMode(),
+            LockerType.Adrenaline => ObjectType.AdrenalineLocker.GetObjectByMode(),
+            _ => null,
+        };
     }
 }
