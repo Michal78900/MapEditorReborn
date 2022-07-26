e37a8b7e03ea1774f183ac8406d4ad0c6f3c0a3e
Michal78900 <michal.dlugopis@gmail.com>
2022-07-26 21:17:00 +0200

    TPS command

---
+++ MapEditorReborn/Commands/TpsCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace MapEditorReborn.Commands
+{
+    using System;
+    using CommandSystem;
+    using Exiled.API.Features;
+
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class TpsCommand : ICommand
+    {
+        public string Command { get; } = "tps";
+
+        public string[] Aliases { get; } = Array.Empty<string>();
+
+        public string Description { get; } = "Shows the current TPS.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            double diff = Server.Tps / ServerStatic.ServerTickrate;
+            string color = diff switch
+            {
+                > 0.9 => "green",
+                > 0.5 => "yellow",
+                _ => "red"
+            };
+
+            response = $"<color={color}>{Server.Tps}/{ServerStatic.ServerTickrate}</color>";
+            return true;
+        }
+    }
+}
