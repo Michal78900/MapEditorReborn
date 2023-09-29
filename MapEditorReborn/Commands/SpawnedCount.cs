a48df46a896d1a85f96381bfb23a4275da0352cf
JasKill <jaskll000@gmail.com>
2023-09-29 23:03:08 +0700

    feat: команда для подсчета объектов

---
+++ MapEditorReborn/Commands/SpawnedCount.cs
@@ -0,0 +1,19 @@
+﻿namespace MapEditorReborn.Commands;
+
+using System;
+using CommandSystem;
+
+public class SpawnedCount : ICommand
+{
+    public string Command => "spawnedcount";
+
+    public string[] Aliases { get; } = Array.Empty<string>();
+
+    public string Description => "Количество заспавленых объектов";
+
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        response = $"Заспавлено объектов - {API.API.SpawnedObjects.Count}";
+        return true;
+    }
+}
