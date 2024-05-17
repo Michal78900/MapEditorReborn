09bb325eab846752e54c6b7f5471c8d5f262cbd8
Michal78900 <Michal78900@users.noreply.github.com>
2024-05-18 00:22:23 +0200

    Version update

--- MapEditorReborn/MapEditorReborn.cs
+++ MapEditorReborn/MapEditorReborn.cs
@@ -215,9 +215,9 @@ namespace MapEditorReborn
         public override string Author => "Michal78900";
 
         /// <inheritdoc/>
-        public override Version Version { get; } = new (3, 1, 0);
+        public override Version Version { get; } = new (3, 2, 0);
 
         /// <inheritdoc/>
-        public override Version RequiredExiledVersion { get; } = new (8, 7, 0);
+        public override Version RequiredExiledVersion { get; } = new (8, 9, 0);
     }
 }
