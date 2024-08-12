80f529054d3a17ae1a9a572f006624a82cf1d572
Izzy4ka <baielaibek@mail.ru>
2024-08-13 02:05:03 +0800

    chore: bump 3.1.9

--- MapEditorReborn/MapEditorReborn.cs
+++ MapEditorReborn/MapEditorReborn.cs
@@ -215,7 +215,7 @@ namespace MapEditorReborn
         public override string Author => "Michal78900, NekoDev Team";
 
         /// <inheritdoc/>
-        public override Version Version { get; } = new (3, 1, 8);
+        public override Version Version { get; } = new (3, 1, 9);
 
         /// <inheritdoc/>
         public override Version RequiredExiledVersion { get; } = new (8, 9, 5);
