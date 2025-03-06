8e5f1d225c8bc316470d8553892cd73f5df2900c
R2kip <56793105+R2kip@users.noreply.github.com>
2025-03-07 00:40:58 +0300

    Optimization

--- MapEditorReborn/Patches/UpdatePositionServerPatch.cs
+++ MapEditorReborn/Patches/UpdatePositionServerPatch.cs
@@ -17,6 +17,7 @@ namespace MapEditorReborn.Patches
     {
         private static bool Prefix(AdminToyBase __instance)
         {
+            if (__instance.IsStatic) return false;
             __instance.NetworkPosition = __instance.transform.position;
             __instance.NetworkRotation = __instance.transform.rotation;
             __instance.NetworkScale = __instance.transform.root != __instance.transform ? Vector3.Scale(__instance.transform.localScale, __instance.transform.root.localScale) : __instance.transform.localScale;
